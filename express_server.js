70bf6b4d245fc68bc4c6b4afa0dce610578c6d40
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-12-06 19:15:56 -0500

    no change

--- express_server.js
+++ express_server.js
@@ -15,7 +15,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
