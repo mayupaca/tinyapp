94123974fa5ec190471638748e59ab4c1727801a
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-12-07 19:01:06 -0500

    day1 exercises done

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+// "view engine" プロパティはどのテンプレートエンジンを使うかの指定を行うプロパティ
+//
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
@@ -12,9 +13,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+const generateRandomString = function () {
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randStr = "";
+  for (var i = 0; i < 6; i++) {
+    randStr += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return randStr;
+};
+
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  console.log("urls req.body", req.body); // Log the POST request body to the console
+  res.redirect("/urls"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls", (req, res) => {
@@ -30,8 +43,10 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
 app.get("/u/:shortURL", (req, res) => {
-  // const longURL = ...
+  const longURL = urlDatabase[req.params.shortURL];
+  console.log(req.params);
   res.redirect(longURL);
 });
 
@@ -51,6 +66,3 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-// const generateRandomString = function () {
-//   
-// };
