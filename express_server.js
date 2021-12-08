8130609ed1179c03eccfefc504d3df277fa57a9a
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-12-07 20:31:32 -0500

    Add delete buttoms

--- express_server.js
+++ express_server.js
@@ -22,6 +22,12 @@ const generateRandomString = function () {
   return randStr;
 };
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   const longURL = req.body.longURL;
