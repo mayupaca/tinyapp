e1c2880120ca62e62fb5b69f5b224fd47f8add75
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-12-08 20:09:13 -0500

    message

--- express_server.js
+++ express_server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 // "view engine" プロパティはどのテンプレートエンジンを使うかの指定を行うプロパティ
-//
+
 app.set("view engine", "ejs");
 
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -23,11 +24,13 @@ const generateRandomString = function () {
   return randStr;
 };
 // Pass in the username to all views
-const templateVars = {
-  username: req.cookies["username"],
-  
-};
-res.render("urls_index", templateVars);
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase,
+  };
+  res.render("urls_index", templateVars);
+});
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
@@ -45,21 +48,29 @@ app.post("/urls", (req, res) => {
 
 app.post('/login', (req, res) => {
   const username = req.body.username;
-  res.cookie("name", username);
+  res.cookie("username", username);
+  res.redirect("/urls");
+  console.log(req.body)
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
   res.render("urls_show", templateVars);
 });
 
